dc35c6f428dae5518d2272e7c7aeb76f5bff5a58
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-05-23 11:44:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,15 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
-╭──『 𝗗𝗢𝗡𝗔𝗦𝗜 』──⬣
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(image + 'donasi')).buffer(), `
+╭──『 𝘿𝙤𝙣𝙖𝙨𝙞 • 𝙀-𝙢𝙤𝙣𝙚𝙮 』──⬣
 │⬣ 𝘋𝘢𝘯𝘢 : 088233832771
 │⬣ 𝘎𝘰𝘱𝘢𝘺 : 088233832771
 ╰────────⬣
+╭──「 𝙉𝙊𝙏𝙀 」──⬣
+│ > Owner? wa.me/6288233832771
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24jam tanpa kendala_
+╰──────────⬣
 `.trim(), footer, 'Owner', '.owner', 'Menu', '.menu')
 handler.help = ['donasi']
 handler.tags = ['info']

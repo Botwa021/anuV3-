17255d84f29b08281fdb23b6669c14c871e935b0
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-05-23 09:40:50 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,27 +1,8 @@
 let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Pulsa
-╠➥ Ke nomer ini
-║➥ 081360482998
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
+╠═ UNTUK PEMBAYARAN VIA PULSA
+║ ⬣ ISI SENDIRI TIDAK MELAYANI VIA PULSA
+║ ⬣ SAAT DI TRANSFER PULSA KE SEDOT 
 ╠═ ©2021 Rpg wabot-aq
 ╠═ Scrip original by Nurutomo
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
